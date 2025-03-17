216fa52af5f8f4d3acdc128cb3f27a771fc7cdf1
Pruthviraj Chavan <chavanpruthviraj9595@gmail.com>
2025-03-17 22:20:56 +0530

    final push

--- backend/db.js
+++ backend/db.js
@@ -1,26 +1,27 @@
-const mongoose = require('mongoose')
-// const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb://<!#@!#@>:"!@!@"@mernclustedsfhcbshdbf$##################,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/Customer?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority' // Customer change url to your db you created in atlas
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI;
 
 module.exports = function (callback) {
-    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
-        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
-        if (err) console.log("---" + err)
-        else {
-            // var database =
-            console.log("connected to mongo")
-            const foodCollection = await mongoose.connection.db.collection("food_items");
-            foodCollection.find({}).toArray(async function (err, data) {
-                const categoryCollection = await mongoose.connection.db.collection("Categories");
-                categoryCollection.find({}).toArray(async function (err, Catdata) {
-                    callback(err, data, Catdata);
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(async () => {
+            console.log("Connected to MongoDB");
 
-                })
-            });
-            // listCollections({name: 'food_items'}).toArray(function (err, database) {
-            // });
-            //     module.exports.Collection = database;
-            // });
-        }
-    })
+            try {
+                const foodCollection = mongoose.connection.db.collection("food_items");
+                const data = await foodCollection.find({}).toArray();
+
+                const categoryCollection = mongoose.connection.db.collection("Categories");
+                const Catdata = await categoryCollection.find({}).toArray();
+
+                callback(null, data, Catdata);
+            } catch (error) {
+                console.error("Error fetching collections:", error);
+                callback(error, null, null);
+            }
+        })
+        .catch(err => {
+            console.error("MongoDB Connection Error:", err);
+        });
 };
